452f4336ff9c6b9096842b1d8372f15453f7bee6
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2019-08-26 16:12:09 +0900

    1st commit
    add XF app and XA app

---
+++ XA_ClearTrafficTest/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.Design.Widget;
+using Android.Webkit;
+
+namespace XA_ClearTrafficTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextInputEditText urlText;
+        WebView webView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            urlText = FindViewById<TextInputEditText>(Resource.Id.urlText);
+            var goButton = FindViewById<Button>(Resource.Id.goButton);
+
+            webView = FindViewById<WebView>(Resource.Id.webView);
+            webView.Settings.JavaScriptEnabled = true;
+            webView.SetWebViewClient(new ClearWebViewClient());
+            webView.LoadUrl("https://docs.microsoft.com/ja-jp/xamarin/android/user-interface/controls/web-view");
+
+            goButton.Click += GoButton_Click;
+        }
+
+        private void GoButton_Click(object sender, System.EventArgs e)
+        {
+            webView.LoadUrl(urlText.Text);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public class ClearWebViewClient : WebViewClient
+        {
+            public override bool ShouldOverrideUrlLoading(WebView view, string url)
+            {
+                view.LoadUrl(url);
+                return false;
+            }
+        }
+    }
+}
---
+++ XF_ClearTrafficTest/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF_ClearTrafficTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            goButton.Clicked += GoButton_Clicked;
+        }
+
+        private void GoButton_Clicked(object sender, EventArgs e)
+        {
+            webView.Source = urlText.Text;
+        }
+    }
+}
