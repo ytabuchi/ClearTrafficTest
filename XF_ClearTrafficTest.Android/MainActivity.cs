07497c588b2db498b69413ef4b4145612e2b1903
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2019-08-26 16:19:34 +0900

    fix XF app name

--- XF_ClearTrafficTest.Android/MainActivity.cs
+++ XF_ClearTrafficTest.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace XF_ClearTrafficTest.Droid
 {
-    [Activity(Label = "XF_ClearTrafficTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "XF ClearTrafficTest App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
